0d785ca9dc2c2d0a567a5506858922caade947a1
Greentea <vckuznetsov@gmail.com>
2024-04-11 22:13:27 +0300

    Fixed MSVC/Win version; added README

--- regen_cmake.sh
+++ regen_cmake.sh
@@ -8,7 +8,7 @@ BUILD_TYPE="${BUILD_TYPE:-${1:-Release}}"
 echo "Using BUILD_TYPE=$BUILD_TYPE"
 export EXPORT_COMPILE_COMMANDS="${EXPORT_COMPILE_COMMANDS:-$CCMDS}"
 if [ "$EXPORT_COMPILE_COMMANDS" == "0" ]; then
-    unset EXPORT_COMPILE_COMMANDS
+	unset EXPORT_COMPILE_COMMANDS
 fi
 echo "Using EXPORT_COMPILE_COMMANDS=$EXPORT_COMPILE_COMMANDS"
 
@@ -21,6 +21,6 @@ cmake -S . -B Build/Debug -DCMAKE_BUILD_TYPE=Debug
 # cmake -S . -B Build/$BUILD_TYPE -DCMAKE_BUILD_TYPE=$BUILD_TYPE
 
 if [ ! -z "$EXPORT_COMPILE_COMMANDS" ]; then
-    echo "Making symlink to Build/$BUILD_TYPE/compile_commands.json"
-    ln -s Build/$BUILD_TYPE/compile_commands.json .
+	echo "Making symlink to Build/$BUILD_TYPE/compile_commands.json"
+	ln -s Build/$BUILD_TYPE/compile_commands.json .
 fi
--- run.bat
+++
@@ -1,3 +0,0 @@
-@echo "I don't know how to deal with batch scripts yet"
-@echo "Do It Yourself"
-exit 1
