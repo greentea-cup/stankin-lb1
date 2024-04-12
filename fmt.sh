570676afb0eac0b88678d76c1ff42ed197e7b082
Greentea <vckuznetsov@gmail.com>
2024-04-12 09:38:16 +0300

    Changed formatter args

--- fmt.sh
+++ fmt.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-find "src" "include" -type f -regextype posix-extended -iregex ".*\.(c|cxx|cpp|h|hpp)" | xargs astyle -Tym0xUoO
+find "src" "include" -type f -regextype posix-extended -iregex ".*\.(c|cxx|cpp|h|hpp)" | xargs astyle -n -T -y -m0 -xU -O -H -p -xg -p -U -L
 
