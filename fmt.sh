49e43aa69809efec659ee99fa64ef21fafdf5497
Greentea <vckuznetsov@gmail.com>
2024-04-17 21:51:54 +0300

    Changed formatter args

--- fmt.sh
+++ fmt.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-find "src" "include" -type f -regextype posix-extended -iregex ".*\.(c|cxx|cpp|h|hpp)" | xargs astyle -n -T -y -m0 -xU -O -H -p -xg -p -U -L
+find "src" "include" -type f -regextype posix-extended -iregex ".*\.(c|cxx|cpp|h|hpp)" | xargs astyle \
+	-n -T4 -y -m0 -o -O -xU -H -p -xg -p -U -L -xC100 -xL -k3 --squeeze-ws --squeeze-lines=1 -w
 
